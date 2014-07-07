93a3e410f820a14253d7802574eca6e574d24a8e
kmrz <krzadca@mimuw.edu.pl>
2014-07-07 11:25:28 +0200

    automatically run tests for multiple workloads

---
+++ testrunner/testrunner.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+import glob
+import logging
+
+simconfig = """
+--title          %s
+--job_id         401
+--block_time     3
+--block_margin   0
+--one_block      True
+--serial         0
+--cpu_count      0
+--cpu_percent    70
+--cpu_per_node   0
+--output         testrunner_results
+
+--threshold      10
+--decay          1
+--default_limit  7
+--share_file     shares.txt
+--bf_depth       50
+--bf_window      24
+--bf_interval    5
+
+--estimator      %s
+--submitter      %s
+--selector       VirtualSelector
+--schedulers     %s
+--share          EqualShare
+"""
+
+
+def conf_clairvoyant(tracename):
+    curr_config = simconfig % ("trun-clairvoyant-"+tracename, "NaiveEstimator", "OracleSubmitter", "OStrich Fairshare")
+    with open("clairvoyant_conf", "w") as f:
+        f.write(curr_config)
+    return "clairvoyant_conf"
+
+def conf_nonclairvoyant(tracename):
+    curr_config = simconfig % ("trun-nonclairvoyant-"+tracename, "PreviousN", "FromWorkloadSubmitter", "OStrich")
+    with open("nonclairvoyant_conf", "w") as f:
+        f.write(curr_config)
+    return "nonclairvoyant_conf"
+
+def run(*command):
+    logging.info("running: \n"+" ".join(command))
+    subprocess.call(command)
+
+def test_trace(tracedir, tracename):
+    conf_clairvoyant(tracename)
+    conf_nonclairvoyant(tracename)
+    run("python", "main.py", "run", tracedir+tracename+".swf", "@clairvoyant_conf")
+    run("python", "main.py", "run", tracedir+tracename+".swf", "@nonclairvoyant_conf")
+
+def draw_trace(tracename):
+    fsfile = glob.glob("testrunner_results/trun-clairvoyant-"+tracename+"-Fairshare-*")[0]
+    logging.info("fairshare file: %s" % fsfile)
+    clairfile = glob.glob("testrunner_results/trun-clairvoyant-"+tracename+"-OStrich-*")[0]
+    logging.info("clairvoyant ostrich file: %s" % clairfile)
+    unclairfile = glob.glob("testrunner_results/trun-nonclairvoyant-"+tracename+"-OStrich-*")[0]
+    logging.info("non-clairvoyant ostrich file: %s" % unclairfile)
+    run("python", "drawing/draw_graphs.py", "--output", "testrunner_results/"+tracename, "--minlen", "30", fsfile, clairfile, unclairfile)
+
+if __name__ == "__main__":
+    try:
+        os.mkdir("testrunner_results")
+    except OSError:
+        pass
+    logging.basicConfig(level=logging.INFO)
+
+    traces = ["ANL-Intrepid-2009-1", "CEA-Curie-2011-2", "METACENTRUM-2009-2", "PIK-IPLEX-2009-1", "RICC-2010-2",]
+    tracedir = "../traces/"
+
+    for trace in traces:
+        test_trace(tracedir, trace)
+        draw_trace(trace)
